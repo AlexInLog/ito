ae8674c90b973cb007686325f6acd4e2b6086a2c
Aleksey Loginov <victimsnino@gmail.com>
2026-07-05 23:14:02 +0300

    style

--- include/ito/coro.hpp
+++ include/ito/coro.hpp
@@ -140,7 +140,7 @@ namespace ito
             {
                 throw exceptions::invalid_coro_handle_state{"no coroutine handle when trying to run coro"};
             }
-            
+
             m_h.resume();
             const auto _ = utils::finally{[this]() noexcept { std::exchange(m_h, {}).destroy(); }};
             return m_h.promise().get_result();
