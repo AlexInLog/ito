ccd204f9ccde758447c1364e8deca0e2964d9c23
Aleksey Loginov <32845901+AlexInLog@users.noreply.github.com>
2026-07-07 00:11:20 +0300

    improve coverage (#8)
    * improve coverage
    
    * improve coverage

--- include/ito/loop.hpp
+++ include/ito/loop.hpp
@@ -11,26 +11,36 @@ namespace ito
 {
     class loop
     {
+        auto lock()
+        {
+            auto& current_loop_ptr = current_impl();
+            if (current_loop_ptr)
+            {
+                throw exceptions::invalid_loop_state{
+                    "loop::run_until_complete() called from within an already-running loop on this thread"
+                };
+            }
+
+            current_loop_ptr = this;
+            return utils::finally{[]() noexcept { current_impl() = nullptr; }};
+        }
+
+        static loop*& current_impl()
+        {
+            static thread_local loop* s_loop{};
+            return s_loop;
+        }
+
     public:
         loop() = default;
 
         template<typename T>
         T run_until_complete(ito::coro<T>&& coro)
         {
-            auto& current_loop_ptr = current_impl();
-            if (current_loop_ptr)
-            {
-                throw exceptions::invalid_loop_state{"loop::run_until_complete() called from within an already-running loop on this thread"};
-            }
-
-            current_loop_ptr                 = this;
-            const auto free_current_loop_ptr = utils::finally{[]() noexcept { current_impl() = nullptr; }};
+            [[maybe_unused]] const auto locked = lock();
 
             std::coroutine_handle<typename ito::coro<T>::promise_type> h = std::move(coro).detach();
-            if (!h.done())
-            {
-                h.resume();
-            }
+            h.resume();
 
             const auto _ = utils::finally{[&h]() noexcept { h.destroy(); }};
             return h.promise().get_result();
@@ -44,10 +54,5 @@ namespace ito
         // static loop* try_current() noexcept { return current_impl(); }
 
     private:
-        static loop*& current_impl()
-        {
-            static thread_local loop* s_loop{};
-            return s_loop;
-        }
     };
 } // namespace ito
