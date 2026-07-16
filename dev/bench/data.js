99d78cf63cc8d217da1ce849bd7a126ad528b17e
github-action-benchmark <github@users.noreply.github.com>
2026-07-16 20:57:46 +0000

    add Ito benchmarks - Ubuntu - clang (catch2) benchmark result for 7b0a3b9be8b8eb9b82fd8111f469cd77df50b95b

---
+++ dev/bench/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1784235466607,
+  "repoUrl": "https://github.com/AlexInLog/ito",
+  "entries": {
+    "Ito benchmarks - Ubuntu - clang": [
+      {
+        "commit": {
+          "author": {
+            "email": "32845901+AlexInLog@users.noreply.github.com",
+            "name": "Aleksey Loginov",
+            "username": "AlexInLog"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7b0a3b9be8b8eb9b82fd8111f469cd77df50b95b",
+          "message": "Update build.yml",
+          "timestamp": "2026-07-16T23:55:59+03:00",
+          "tree_id": "13e2f1a3f6f6db5d95ec134f3c8fafe2d5253a6d",
+          "url": "https://github.com/AlexInLog/ito/commit/7b0a3b9be8b8eb9b82fd8111f469cd77df50b95b"
+        },
+        "date": 1784235466397,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "call no-op coro",
+            "value": 19.6662,
+            "range": "± 0.892856",
+            "unit": "ns",
+            "extra": "100 samples\n1544 iterations"
+          }
+        ]
+      }
+    ]
+  }
+}
