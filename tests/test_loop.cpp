da9f270b4aa02ebf3945a4f080290b56cd17a647
Aleksey Loginov <victimsnino@gmail.com>
2026-07-16 23:49:49 +0300

    extend tests

--- tests/test_loop.cpp
+++ tests/test_loop.cpp
@@ -1,7 +1,11 @@
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/trompeloeil.hpp>
 #include <ito/coro.hpp>
 #include <ito/exceptions.hpp>
 #include <ito/loop.hpp>
+#include <trompeloeil/mock.hpp>
+
+#include <coroutine>
 
 TEST_CASE("can't run loop inside loop")
 {
@@ -29,4 +33,46 @@ TEST_CASE("can't run same loop inside loop")
         );
         co_return;
     }());
+}
+
+class call_mock
+{
+    MAKE_MOCK(call, auto(size_t)->void);
+};
+
+TEST_CASE("call_soon relatively to run_until_complete")
+{
+    ito::loop loop{};
+    call_mock mock{};
+
+    SECTION("no-op coro")
+    {
+        loop.call_soon([&]() { mock.call(1); });
+
+        REQUIRE_CALL(mock, call(1));
+
+        loop.run_until_complete([&]() -> ito::coro<> {
+            loop.call_soon([&]() { mock.call(2); });
+            co_return;
+        }());
+
+        loop.call_soon([&]() { mock.call(3); });
+    }
+
+    SECTION("suspending coro")
+    {
+        loop.call_soon([&]() { mock.call(1); });
+
+        REQUIRE_CALL(mock, call(1));
+
+        REQUIRE_THROWS_AS(loop.run_until_complete([&]() -> ito::coro<> {
+            loop.call_soon([&]() { mock.call(2); });
+            REQUIRE_CALL(mock, call(2));
+
+            co_await std::suspend_always{};
+            co_return;
+        }()), ito::exceptions::empty_value);
+
+        loop.call_soon([&]() { mock.call(3); });
+    }
 }
