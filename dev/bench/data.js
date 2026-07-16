c3b03ae121425edc573aa20121034ab2cee6463d
github-action-benchmark <github@users.noreply.github.com>
2026-07-16 21:09:23 +0000

    add Ito benchmarks - Ubuntu - clang (catch2) benchmark result for 55f3999b07924cf28a98d8179a6a5ce4fc5037f3

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1784235466607,
+  "lastUpdate": 1784236163300,
   "repoUrl": "https://github.com/AlexInLog/ito",
   "entries": {
     "Ito benchmarks - Ubuntu - clang": [
@@ -31,6 +31,37 @@ window.BENCHMARK_DATA = {
             "range": "± 0.892856",
             "unit": "ns",
             "extra": "100 samples\n1544 iterations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "32845901+AlexInLog@users.noreply.github.com",
+            "name": "Aleksey Loginov",
+            "username": "AlexInLog"
+          },
+          "committer": {
+            "email": "victimsnino@gmail.com",
+            "name": "Aleksey Loginov",
+            "username": "AlexInLog"
+          },
+          "distinct": true,
+          "id": "55f3999b07924cf28a98d8179a6a5ce4fc5037f3",
+          "message": "Update build.yml",
+          "timestamp": "2026-07-17T00:08:00+03:00",
+          "tree_id": "45209177800e7d396d0b72cee3368f44d9a70c7b",
+          "url": "https://github.com/AlexInLog/ito/commit/55f3999b07924cf28a98d8179a6a5ce4fc5037f3"
+        },
+        "date": 1784236162718,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "call no-op coro",
+            "value": 20.1396,
+            "range": "± 2.30152",
+            "unit": "ns",
+            "extra": "100 samples\n1526 iterations"
           }
         ]
       }
