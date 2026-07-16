1908ced1e1916c87acb90aa1509e1bfb74b8ed7b
github-action-benchmark <github@users.noreply.github.com>
2026-07-16 21:17:09 +0000

    add Ito benchmarks - Ubuntu - clang (catch2) benchmark result for 5a4af2261f7a20e19ab6842b22127a93f7b2268c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1784236163300,
+  "lastUpdate": 1784236628995,
   "repoUrl": "https://github.com/AlexInLog/ito",
   "entries": {
     "Ito benchmarks - Ubuntu - clang": [
@@ -62,6 +62,44 @@ window.BENCHMARK_DATA = {
             "range": "± 2.30152",
             "unit": "ns",
             "extra": "100 samples\n1526 iterations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "32845901+AlexInLog@users.noreply.github.com",
+            "name": "Aleksey Loginov",
+            "username": "AlexInLog"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5a4af2261f7a20e19ab6842b22127a93f7b2268c",
+          "message": "Extend bench (#16)\n\n* Update build.yml\n\n* extend bench",
+          "timestamp": "2026-07-17T00:16:00+03:00",
+          "tree_id": "f9ba00dd90794f34b1dcf669d8dc716b18d02005",
+          "url": "https://github.com/AlexInLog/ito/commit/5a4af2261f7a20e19ab6842b22127a93f7b2268c"
+        },
+        "date": 1784236628750,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "call no-op coro",
+            "value": 19.3319,
+            "range": "± 0.981979",
+            "unit": "ns",
+            "extra": "100 samples\n1464 iterations"
+          },
+          {
+            "name": "call no-op coro as child",
+            "value": 35.9409,
+            "range": "± 2.0125",
+            "unit": "ns",
+            "extra": "100 samples\n815 iterations"
           }
         ]
       }
