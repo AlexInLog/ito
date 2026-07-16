aeec678b05a0a47438ee26db14dcd6fd0858910c
Aleksey Loginov <victimsnino@gmail.com>
2026-07-16 23:12:35 +0300

    add benchmarks

---
+++ benchmarks/benchmark_base_loop.cpp
@@ -0,0 +1,15 @@
+#include "ito/coro.hpp"
+#include "ito/loop.hpp"
+
+#include <catch2/benchmark/catch_benchmark.hpp>
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("no-op loop checks")
+{
+    BENCHMARK("call no-op coro")
+    {
+        return ito::loop{}.run_until_complete([]() -> ito::coro<> {
+            co_return;
+        }());
+    }; // BENCHMARK("call no-op coro")
+}
