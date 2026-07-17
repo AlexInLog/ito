076205ec0d7e1137da6f81a22fe9d691c5272073
Aleksey Loginov <victimsnino@gmail.com>
2026-07-17 23:00:24 +0300

    revert back

--- include/ito/details/utils/raii_coro_handle.hpp
+++ include/ito/details/utils/raii_coro_handle.hpp
@@ -32,7 +32,6 @@ namespace ito::details::utils
 
     protected:
         [[nodiscard]] std::coroutine_handle<> get_impl() const & { return m_h; }
-        [[nodiscard]] std::coroutine_handle<> get_impl()  && { return std::exchange(m_h, {}); }
 
     private:
         std::coroutine_handle<> m_h{};
@@ -52,11 +51,6 @@ namespace ito::details::utils
         [[nodiscard]] std::coroutine_handle<TPromise> get() const
         {
             return std::coroutine_handle<TPromise>::from_address(get_impl().address());
-        }
-
-        [[nodiscard]] std::coroutine_handle<TPromise> detach() &&
-        {
-            return std::coroutine_handle<TPromise>::from_address(std::move(*this).get_impl().address());
         }
     };
 } // namespace ito::details::utils
