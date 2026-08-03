35e848d56e6a76bdef89b2b4a82b24ad71540b87
Aleksey Loginov <victimsnino@gmail.com>
2026-08-04 00:12:44 +0300

    fix

--- include/ito/async/future.hpp
+++ include/ito/async/future.hpp
@@ -6,6 +6,7 @@
 #include <ito/loop.hpp>
 
 #include <coroutine>
+#include <exception>
 #include <utility>
 
 namespace ito::exceptions
@@ -41,10 +42,11 @@ namespace ito::async
         public:
             ~promise_base() noexcept
             {
-                if (!m_value->continuation) [[likely]]
+                const auto ptr = m_value.get();
+                if (!ptr) [[likely]]
                     return;
 
-                notify_broken_future();
+                notify_broken_future(ptr);
             }
 
             // The move constructor is intentionally left defaulted: all state that the destructor
@@ -59,22 +61,32 @@ namespace ito::async
             template<typename... TT>
             void set_result_impl(TT&&... v)
             {
-                auto prepared = this->prepare_scheduling_continuation();
-                m_value->value.set_result(std::forward<TT>(v)...);
-                prepared();
+                if (const auto ptr = m_value.get())
+                {
+                    auto prepared = this->prepare_scheduling_continuation();
+                    ptr->value.set_result(std::forward<TT>(v)...);
+                    prepared();
+                }
             }
 
             void set_exception(const std::exception_ptr& err)
             {
-                auto prepared = this->prepare_scheduling_continuation();
-                m_value->value.set_exception(err);
-                prepared();
+                if (const auto ptr = m_value.get())
+                {
+                    auto prepared = this->prepare_scheduling_continuation();
+                    ptr->value.set_exception(err);
+                    prepared();
+                }
             }
 
-            [[nodiscard]] bool is_ready() const { return m_value->value.is_ready(); }
+            [[nodiscard]] bool is_ready() const
+            {
+                const auto ptr = m_value.get();
+                return !ptr || ptr->value.is_ready();
+            }
 
         protected:
-            explicit promise_base(ito::details::utils::trackable<details::future_state<T>>&& value)
+            explicit promise_base(ito::details::utils::trackable<details::future_state<T>>::weak_view&& value)
                 : m_value{std::move(value)}
             {
             }
@@ -83,15 +95,25 @@ namespace ito::async
             // The broken-future notification is kept out of ~promise_base(): it only runs when the
             // promise dies with a coroutine still waiting on it, and inlining it (call_soon() plus the
             // try/catch) is what stops the destructor itself from being inlined into its caller.
-            void notify_broken_future() noexcept
+            void notify_broken_future(details::future_state<T>* ptr) noexcept
             {
+                if (!ptr->value.is_ready()) [[unlikely]]
+                {
+                    ptr->value.set_exception(
+                        std::make_exception_ptr(ito::exceptions::broken_future{"promise object destroyed unresolved"})
+                    );
+                }
+
+                if (!ptr->continuation) [[likely]]
+                    return;
+
                 if (const auto loop = ito::loop::try_current()) [[likely]]
                 {
                     // call_soon() can throw (e.g. std::bad_alloc from the queue); swallow it
                     // rather than let it escape this noexcept destructor and terminate()
                     try
                     {
-                        loop->call_soon(std::move(m_value->continuation).detach());
+                        loop->call_soon(std::move(ptr->continuation).detach());
                     }
                     catch (...) // NOLINT(bugprone-empty-catch)
                     {
@@ -103,14 +125,15 @@ namespace ito::async
             [[nodiscard]] auto prepare_scheduling_continuation()
             {
                 // we are doing it as lambda to try to catch loop BEFORE actual value changes so crash would happen BEFORE
-                return [loop = m_value->continuation ? &ito::loop::current() : nullptr, this]() {
+                const auto ptr = m_value.get();
+                return [loop = ptr && ptr->continuation ? &ito::loop::current() : nullptr, ptr]() {
                     if (loop)
-                        loop->call_soon(std::move(m_value->continuation).detach());
+                        loop->call_soon(std::move(ptr->continuation).detach());
                 };
             }
 
         private:
-            ito::details::utils::trackable<details::future_state<T>> m_value{};
+            ito::details::utils::trackable<details::future_state<T>>::weak_view m_value{};
         };
     } // namespace details
 
@@ -127,18 +150,14 @@ namespace ito::async
         {
             struct awaitable
             {
-                ito::details::utils::trackable<details::future_state<T>>::weak_view view;
+                ito::details::utils::trackable<details::future_state<T>> obj;
 
-                explicit awaitable(ito::details::utils::trackable<details::future_state<T>>::weak_view&& view)
-                    : view{std::move(view)}
+                explicit awaitable(ito::details::utils::trackable<details::future_state<T>>&& obj)
+                    : obj{std::move(obj)}
                 {
                 }
 
-                ~awaitable() noexcept
-                {
-                    if (const auto ptr = view.get())
-                        ptr->continuation = ito::details::utils::coroutine_handle<>{};
-                }
+                ~awaitable() noexcept { obj->continuation = ito::details::utils::coroutine_handle<>{}; }
 
                 awaitable(const awaitable&)            = delete;
                 awaitable& operator=(const awaitable&) = delete;
@@ -146,38 +165,22 @@ namespace ito::async
                 awaitable(awaitable&& o) noexcept = delete;
                 awaitable& operator=(awaitable&&) = delete;
 
-                constexpr bool await_ready() noexcept
-                {
-                    const auto ptr = view.get();
-                    return !ptr || ptr->value.is_ready();
-                }
+                constexpr bool await_ready() noexcept { return obj->value.is_ready(); }
 
-                auto await_suspend(std::coroutine_handle<> h)
-                {
-                    if (const auto ptr = view.get()) [[likely]]
-                        ptr->continuation = ito::details::utils::coroutine_handle<>{h};
-                    else
-                        details::throw_broken_future();
-                }
+                auto await_suspend(std::coroutine_handle<> h) noexcept { obj->continuation = ito::details::utils::coroutine_handle<>{h}; }
 
-                T await_resume()
-                {
-                    if (const auto ptr = view.get()) [[likely]]
-                        return ptr->value.get_result();
-
-                    details::throw_broken_future();
-                }
+                T await_resume() { return obj->value.get_result(); }
             };
-            return awaitable{std::move(m_view)};
+            return awaitable{std::move(m_obj)};
         }
 
     private:
-        explicit future(ito::details::utils::trackable<details::future_state<T>>::weak_view&& view)
-            : m_view{std::move(view)}
+        explicit future(ito::details::utils::trackable<details::future_state<T>>&& obj)
+            : m_obj{std::move(obj)}
         {
         }
 
-        ito::details::utils::trackable<details::future_state<T>>::weak_view m_view;
+        ito::details::utils::trackable<details::future_state<T>> m_obj;
     };
 
     template<typename T>
@@ -187,7 +190,7 @@ namespace ito::async
         [[nodiscard]] static std::pair<promise<T>, future<T>> create()
         {
             auto [obj, view] = ito::details::utils::trackable<details::future_state<T>>::create();
-            return {promise<T>{std::move(obj)}, future<T>{std::move(view)}};
+            return {promise<T>{std::move(view)}, future<T>{std::move(obj)}};
         }
 
         void set_result(const T& v) { this->set_result_impl(v); }
@@ -196,7 +199,7 @@ namespace ito::async
         using details::promise_base<T>::set_exception;
 
     private:
-        explicit promise(ito::details::utils::trackable<details::future_state<T>>&& value)
+        explicit promise(ito::details::utils::trackable<details::future_state<T>>::weak_view&& value)
             : details::promise_base<T>{std::move(value)}
         {
         }
@@ -209,7 +212,7 @@ namespace ito::async
         [[nodiscard]] static std::pair<promise<void>, future<void>> create()
         {
             auto [obj, view] = ito::details::utils::trackable<details::future_state<void>>::create();
-            return {promise<void>{std::move(obj)}, future<void>{std::move(view)}};
+            return {promise<void>{std::move(view)}, future<void>{std::move(obj)}};
         }
 
         void set_result() { this->set_result_impl(); }
@@ -217,7 +220,7 @@ namespace ito::async
         using details::promise_base<void>::set_exception;
 
     private:
-        explicit promise(ito::details::utils::trackable<details::future_state<void>>&& value)
+        explicit promise(ito::details::utils::trackable<details::future_state<void>>::weak_view&& value)
             : details::promise_base<void>{std::move(value)}
         {
         }
